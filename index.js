536f0fc8047fd4b409a0fd6dd01ac6d8551cc99b
Adrian Smith <sfyire@gmail.com>
2016-07-17 19:24:38 +0100

    Prepare booklets, add airbnb style, adds Proof of concept

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log(document.querySelector('.available-balance'));
+const format_money = (s) => s.replace( /[^\d\.]*/g, '');
+const money_avaliable = document.querySelector('.available-balance').textContent;
+const balance = document.querySelector('.balance span').textContent;
+
+
+alert('Money Avaliable: '+money_avaliable+ ' balance: '+balance);
